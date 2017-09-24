ceab3ae2cd61113a8e78c9af9ec7fdff394a5428
bypixelpro <info@pixelpro.es>
2017-09-24 09:23:53 +0200

    1.3-El-tiempo-peticion-asincrona
    Controlamos los errores y el acceso a internet

--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,12 @@
 package es.pixelpro.david.eltiempo;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -26,43 +30,59 @@ public class MainActivity extends AppCompatActivity {
         double longitude = -122.4233;
         String elTiempoUrl = "https://api.darksky.net/forecast/"+apiKey+"/"+latitude+","+longitude;
 
-        OkHttpClient client = new OkHttpClient();
-        Request peticion = new Request.Builder().url(elTiempoUrl).build();
+        //7) Checkeamos la red antes de empezar
+        if (tenemosRed()) {
+            OkHttpClient client = new OkHttpClient();
+            Request peticion = new Request.Builder().url(elTiempoUrl).build();
 
-        // 1) Necesitamos al objeto call para realizar las peticiones
-        Call call = client.newCall(peticion);
-        //2) La ejecutamos con un bloque try *** Response respouesta = call.execute(); ***
+            // 1) Necesitamos al objeto call para realizar las peticiones
+            Call call = client.newCall(peticion);
+            //2) La ejecutamos con un bloque try *** Response respouesta = call.execute(); ***
 
-        //5) Metodo asincrono, callback
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
+            //5) Metodo asincrono, callback
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
 
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-
-
-                try {
-                    Log.v(TAG, response.body().string());
-
-                    if (response.isSuccessful()){
-
-                    }else{
-                        alertUserAboutError();
-                    }
-
-                } catch (IOException e) {
-                    //3) Sacamos un mensaje en el log con la excepcion ocurrida
-                    Log.e(TAG, "Ha ocurrido un error: ", e);
                 }
 
-            }
-        });
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
 
+
+                    try {
+                        Log.v(TAG, response.body().string());
+
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutError();
+                        }
+
+                    } catch (IOException e) {
+                        //3) Sacamos un mensaje en el log con la excepcion ocurrida
+                        Log.e(TAG, "Ha ocurrido un error: ", e);
+                    }
+
+                }
+            });
+        }else {
+            Toast.makeText(this, "No hay conexión a internet", Toast.LENGTH_SHORT ).show();
+        }
         Log.v(TAG, "Ejecutamos el hilo principal");
 
+    }
+
+    private boolean tenemosRed() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkinfo = manager.getActiveNetworkInfo();
+
+        //8 si hay y esta conectado, devolvemos true
+        boolean hayRed = false;
+        if (networkinfo != null && networkinfo.isConnected()){
+            hayRed = true;
+        }
+        return hayRed;
     }
 
     private void alertUserAboutError() {
